ae9aeb9d9ff51bda951a2f7b97eda7819713efbe
Mandy380 <72581127+Mandy380@users.noreply.github.com>
2025-09-30 09:45:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+ function ask_me() {
+      let name = prompt("What is you Name?");
+      let ans = prompt("Do you love playing Game: Yes or No?");
 
+      if (!name) {
+        alert("Please Enter your Name:");
+      }
+      if (!ans) {
+        alert("Please Enter your Ans:");
+      } else {
+        if (ans === "Yes") {
+          alert(`Thank you, ${name}, and I'm glad you Love Games`);
+        } else {
+          alert(`Sorry to here that ${name}`);
+        }
+      }
+    }
+
+    let btn = document.querySelector("button");
+    btn.addEventListener("click", ask_me);
